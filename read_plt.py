40ecbc41802b85c58cb10f1c94c396760bfa3b03
ermakov-ad <74365049+ermakov-ad@users.noreply.github.com>
2023-06-20 01:00:46 +0500

    Add files via upload
    Next version of reading plt files. Change function with write arrays for more comfortable version

--- read_plt.py
+++ read_plt.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import struct
+from scipy.fft import fft, fftfreq, ifftn
 
 # info from parameters.dat file:
 left_boundary_x =   -3.1415
@@ -26,6 +27,9 @@ def construct_file_path(n):
         help_string += '0'
     return path + help_string + str(n) + '.plt'
 
+# return: 
+# x, y, z = np.array[z][y][x]; size = (cells_z + 1)*(cells_y + 1)*(cells_x + 1)
+# u, v, w = np.array[z][y][x]; size = cells_z * cells_y * cells_x
 def get_data_from_file(file_number):
     file = open(construct_file_path(file_number), 'rb')
     # skip header
@@ -50,56 +54,62 @@ def get_data_from_file(file_number):
     #         structured_data.append(float(double_value))
     #         it += 1
 
-    count_1 = (cells_number_x + 1)*(cells_number_y + 1)*(cells_number_z + 1)
-    count_2 = cells_number_x * cells_number_y * cells_number_z
     # read arrays with coordinates and normal variables
-    help_list = []
-    for i in range(0, count_1):
-        value = file.read(8)
-        help_list.append(float(struct.unpack('<d', value)[0]))
-    x = np.array(help_list, dtype='float')
+    x = np.empty([cells_number_z + 1, cells_number_y + 1, cells_number_x + 1], dtype=float)
+    y = np.empty([cells_number_z + 1, cells_number_y + 1, cells_number_x + 1], dtype=float)
+    z = np.empty([cells_number_z + 1, cells_number_y + 1, cells_number_x + 1], dtype=float)
+    p = np.empty([cells_number_z, cells_number_y, cells_number_x], dtype=float)
+    u = np.empty([cells_number_z, cells_number_y, cells_number_x], dtype=float)
+    v = np.empty([cells_number_z, cells_number_y, cells_number_x], dtype=float)
+    w = np.empty([cells_number_z, cells_number_y, cells_number_x], dtype=float)
 
-    help_list = []
-    for i in range(0, count_1):
-        value = file.read(8)
-        help_list.append(float(struct.unpack('<d', value)[0]))
-    y = np.array(help_list, dtype='float')
+    for z_ind in range(0, cells_number_z + 1):
+        for y_ind in range(0, cells_number_y + 1):
+            for x_ind in range(0, cells_number_x + 1):
+                value = file.read(8)
+                x[z_ind][y_ind][x_ind] = float(struct.unpack('<d', value)[0])
 
-    help_list = []
-    for i in range(0, count_1):
-        value = file.read(8)
-        help_list.append(float(struct.unpack('<d', value)[0]))
-    z = np.array(help_list, dtype='float')
+    for z_ind in range(0, cells_number_z + 1):
+        for y_ind in range(0, cells_number_y + 1):
+            for x_ind in range(0, cells_number_x + 1):
+                value = file.read(8)
+                y[z_ind][y_ind][x_ind] = float(struct.unpack('<d', value)[0])
 
-    help_list = []
-    for i in range(0, count_2):
-        value = file.read(8)
-        help_list.append(float(struct.unpack('<d', value)[0]))
-    p = np.array(help_list, dtype='float')
+    for z_ind in range(0, cells_number_z + 1):
+        for y_ind in range(0, cells_number_y + 1):
+            for x_ind in range(0, cells_number_x + 1):
+                value = file.read(8)
+                z[z_ind][y_ind][x_ind] = float(struct.unpack('<d', value)[0])
 
-    help_list = []
-    for i in range(0, count_2):
-        value = file.read(8)
-        help_list.append(float(struct.unpack('<d', value)[0]))
-    u_center = np.array(help_list, dtype='float')
+    for z_ind in range(0, cells_number_z):
+        for y_ind in range(0, cells_number_y):
+            for x_ind in range(0, cells_number_x):
+                value = file.read(8)
+                p[z_ind][y_ind][x_ind] = float(struct.unpack('<d', value)[0])
 
-    help_list = []
-    for i in range(0, count_2):
-        value = file.read(8)
-        help_list.append(float(struct.unpack('<d', value)[0]))
-    v_center = np.array(help_list, dtype='float')
+    for z_ind in range(0, cells_number_z):
+        for y_ind in range(0, cells_number_y):
+            for x_ind in range(0, cells_number_x):
+                value = file.read(8)
+                u[z_ind][y_ind][x_ind] = float(struct.unpack('<d', value)[0])
 
-    help_list = []
-    for i in range(0, count_2):
-        value = file.read(8)
-        help_list.append(float(struct.unpack('<d', value)[0]))
-    w_center = np.array(help_list, dtype='float')
+    for z_ind in range(0, cells_number_z):
+        for y_ind in range(0, cells_number_y):
+            for x_ind in range(0, cells_number_x):
+                value = file.read(8)
+                v[z_ind][y_ind][x_ind] = float(struct.unpack('<d', value)[0])
+
+    for z_ind in range(0, cells_number_z):
+        for y_ind in range(0, cells_number_y):
+            for x_ind in range(0, cells_number_x):
+                value = file.read(8)
+                w[z_ind][y_ind][x_ind] = float(struct.unpack('<d', value)[0])
 
     if len(file.read()) > 0:
         print("remained " + str(len(file.read())) + " bytes in the end of the file")
         print("something doesn't work")
     file.close()
-    return x, y, z, p, u_center, v_center, w_center
+    return x, y, z, p, u, v, w
 
 x = []
 y = []
@@ -110,7 +120,7 @@ v_center = []
 w_center = []
 # chose time interval
 start_time = 0.0
-end_time = 0.003
+end_time = 0.002
 
 print(int(start_time / time_step))
 print(int(end_time / time_step + 1))
@@ -124,7 +134,7 @@ for i in range(int(start_time / time_step), int(end_time / time_step + 1), 1):
     u_center.append(ans[4])
     v_center.append(ans[5])
     w_center.append(ans[6])
-    print("end " + str(i) + " itteration")
+    print("end " + str(i) + " time itteration")
 
 x = np.array(x)
 y = np.array(y)
@@ -161,8 +171,27 @@ def coordinates_to_index(x_coordinate, y_coordinate, z_coordinate):
     x_index = int((x_coordinate - left_boundary_x) / dx)
     return z_index + y_index + x_index
 
-ind_data = coordinates_to_index(left_boundary_x, down_boundary_y, 1.0)
-picture = []
+print("zero: ")
+print(u_center[0][int(cells_number_z/2)][0:5][0:5])
+print(v_center[0][int(cells_number_z/2)][0:5][0:5])
+print(w_center[0][int(cells_number_z/2)][0:5][0:5])
+print("one: ")
+print(u_center[1][int(cells_number_z/2)][0:5][0:5])
+print(v_center[1][int(cells_number_z/2)][0:5][0:5])
+print(w_center[1][int(cells_number_z/2)][0:5][0:5])
+print("two: ")
+print(u_center[2][int(cells_number_z/2)][0:5][0:5])
+print(v_center[2][int(cells_number_z/2)][0:5][0:5])
+print(w_center[2][int(cells_number_z/2)][0:5][0:5])
+
+after_f = ifftn(u_center[2])
+print(len(after_f[0][0]))
+print(after_f[0:3][0:3][0:3])
+plt.imshow(np.real(after_f[0:-1][0:-1][40]))
+plt.grid()
+plt.show()
+
+""" picture = []
 for i in range(0, 4):
     picture.append(np.empty([cells_number_x, cells_number_y]))
 picture = np.array(picture)
@@ -186,4 +215,4 @@ plt.imshow(picture[2])
 plt.subplot(2, 2, 4)
 plt.imshow(picture[3])
 
-plt.show()
+plt.show() """
