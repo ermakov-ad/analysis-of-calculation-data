9ca22f961157c8db65a5744b06823f4c851ae316
ermakov-ad <74365049+ermakov-ad@users.noreply.github.com>
2023-07-25 22:46:56 +0500

    Add files via upload
    add testing code for calculation parameters A (angle, frequency)

--- read_plt.py
+++ read_plt.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import struct
-from scipy.fft import fftn
+from scipy.fft import fftn, fft, fftfreq
 import math
 
 # info from parameters.dat file:
@@ -215,7 +215,7 @@ def find_full_energy(u, v, w, size_z, size_y, size_x):
 
     return E / (16.0 * math.pi * math.pi * math.pi)
 
-# calculation of frequency arrays along coordinate axes
+# calculation of wave vectors arrays along coordinate axes
 kx = np.arange(cells_number_x // 2)
 ky = np.arange(cells_number_y // 2)
 kz = np.arange(cells_number_z // 4 // 2)
@@ -223,15 +223,8 @@ kz = np.arange(cells_number_z // 4 // 2)
 energy_spectrum = []
 amplitude_wave_vec = modulus_of_vector([cells_number_z // 4 // 2 - 1, cells_number_y // 2 - 1, cells_number_x // 2 - 1], 3)
 
-def find_spectrum_from_file(file_number, count_of_vec_points):
-    ans = get_data_from_file(file_number)
-    x = ans[0]
-    y = ans[1]
-    z = ans[2]
-    p = ans[3]
-    u_center = ans[4]
-    v_center = ans[5]
-    w_center = ans[6]
+def find_velocity_vec(file_number):
+    x, y, z, p, u_center, v_center, w_center = get_data_from_file(file_number)
     print("end reading " + str(file_number) + " time itteration")
 
     # fourier image of velocity components
@@ -242,14 +235,29 @@ def find_spectrum_from_file(file_number, count_of_vec_points):
     fourier_u = fourier_u[:cells_number_z // 4 // 2][:cells_number_y // 2][:cells_number_x // 2]
     fourier_v = fourier_v[:cells_number_z // 4 // 2][:cells_number_y // 2][:cells_number_x // 2]
     fourier_w = fourier_w[:cells_number_z // 4 // 2][:cells_number_y // 2][:cells_number_x // 2]
+    # vector velocity(k) = [fourier_u, fourier_v, fourier_w]
+    return fourier_u, fourier_v, fourier_w
 
+def find_spectrum_from_file(file_number, count_of_vec_points):
+    fourier_u, fourier_v, fourier_w = find_velocity_vec(file_number)
     # find E(q) in this file:
     E_i = find_energy_spectrum(fourier_u, fourier_v, fourier_w, cells_number_z // 4 // 2, cells_number_y // 2, cells_number_x // 2, count_of_vec_points, kz, ky, kx, amplitude_wave_vec)
     max_energy = np.max(E_i)
     q_max = np.argmax(E_i) * amplitude_wave_vec / count_of_vec_points
     full_e = find_full_energy(fourier_u, fourier_v, fourier_w, cells_number_z // 4 // 2, cells_number_y // 2, cells_number_x // 2)
     return E_i, max_energy, q_max, full_e
-    
+
+def spherical_coord_from_vec(x, y, z):
+    xy = x**2 + y**2
+    r = np.sqrt(xy + z**2)
+    theta = np.arctan2(np.sqrt(xy), z) # for elevation angle defined from Z-axis down
+    phi = np.arctan2(y, x)  # for elevation angle defined from XY-plane up
+    return r, theta, phi
+
+u_k = []
+v_k = []
+w_k = []
+
 count_of_vec_steps = 200
 # cycle according to the time we are interested in
 for i in range(0, int(end_time / time_step + 1) - int(start_time / time_step), 1):    
@@ -259,8 +267,39 @@ for i in range(0, int(end_time / time_step + 1) - int(start_time / time_step), 1
     print("maximum spectrum of energy = " + str(maximum_E) + " in wave vector = " + str(Q_max))
     energy_spectrum.append(energy_sp)
 
-# draw energy spectrum
+    vel_vec = find_velocity_vec(i + int(start_time / time_step))
+    u_k.append(vel_vec[0])
+    v_k.append(vel_vec[1])
+    w_k.append(vel_vec[2])
 energy_spectrum = np.array(energy_spectrum)
+
+l = int(end_time / time_step + 1) - int(start_time / time_step)
+u_wk = np.real(fft(u_k))
+v_wk = np.real(fft(v_k))
+w_wk = np.real(fft(w_k))
+u_wk = u_wk[:l//2]
+v_wk = v_wk[:l//2]
+w_wk = w_wk[:l//2]
+freq_ = fftfreq(l, time_step)[:l//2]    # w 
+# v (w, k) = (u, v, w)[w][kz, ky, kx]
+# A_w_theta = summ (v(w, k)^2), where |theta_k - theta| < delta_theta / 2
+delta_theta = 0.15
+count_of_theta = int(math.pi / delta_theta) + 1
+A_w_theta = []
+theta_array = np.linspace(0.0, math.pi, num=count_of_theta)
+for i in range(0, l//2):
+    A = np.zeros(count_of_theta)
+    for ind_z in range(0, cells_number_z // 4 // 2):
+        for ind_y in range(0, cells_number_y // 2):
+            for ind_x in range(0, cells_number_x // 2):
+                for theta_ind in range(0, count_of_theta):
+                    mod_k, theta_k, phi = spherical_coord_from_vec(ind_x, ind_y, ind_z)
+                    if (abs(theta_array[theta_ind] - theta_k) < delta_theta / 2):
+                        A[theta_ind] += sqr_of_vector([u_wk[i][ind_z][ind_y][ind_x], v_wk[i][ind_z][ind_y][ind_x], w_wk[i][ind_z][ind_y][ind_x]], 3)
+    A_w_theta.append(A)
+A_w_theta = np.array(A_w_theta)
+
+# draw energy spectrum
 wave_coord = np.linspace(0, amplitude_wave_vec + max_wave_deviation, count_of_vec_steps)
 
 fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 14))
@@ -291,3 +330,26 @@ axs[1, 1].set_xlabel('lengh of wave vector')
 axs[1, 1].grid(True)
 
 plt.show()
+
+# draw inertial waves
+fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 14))
+fig.suptitle('amplitudes of inertial waves in phase space', fontsize=14)
+
+axs[0].set_title("frequency = " + str(freq_[0]))
+axs[0].plot(theta_array, A_w_theta[0])
+axs[0].set_ylabel('A(frequency, angle)')
+axs[0].set_xlabel('angle (theta)')
+axs[0].grid(True)
+
+axs[1].set_title("frequency = " + str(freq_[1]))
+axs[1].plot(theta_array, A_w_theta[0])
+axs[1].set_ylabel('A(frequency, angle)')
+axs[1].set_xlabel('angle (theta)')
+axs[1].grid(True)
+
+plt.show()
+
+print("frequency = " + str(freq_[0]))
+print("maximum of A = " + str(np.max(A_w_theta[0])) + " in angle "+ str(theta_array[np.argmax(A_w_theta[0])]))
+print("frequency = " + str(freq_[1]))
+print("maximum of A = " + str(np.max(A_w_theta[1])) + " in angle "+ str(theta_array[np.argmax(A_w_theta[1])]))
